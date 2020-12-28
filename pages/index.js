8acf8643d3fc0d02f7f86b756fe348d833f22c41
Saeed Hadikhanloo <saeed.hadikhanloo@gmail.com>
2020-12-28 22:47:54 +0100

    use nextjs Link and Image

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,5 @@
+import Link from 'next/link';
+import Image from 'next/image';
 import styles from '../styles/Home.module.scss';
 
 export default function Home() {
@@ -5,12 +7,22 @@ export default function Home() {
     <div className={styles.container}>
       <div className={styles.navbar}>
         <div className={styles.logo}>
-          <img src="favicon.ico" />
+          <Image src="/favicon.ico" width={500} height={500} />
         </div>
         <div className={styles.navbarItems}>
-          <div className={styles.navbarItem}>Home</div>
-          <div className={styles.navbarItem}>Home</div>
-          <div className={styles.navbarItem}>Home</div>
+          <div className={styles.navbarItem}>
+            <Link href="/">
+              <a>Home</a>
+            </Link>
+          </div>
+          <div className={styles.navbarItem}>
+            <Link href="/about">
+              <a>About</a>
+            </Link>
+          </div>
+          <div className={styles.navbarItem}>
+            <div className={styles.btn}>Login</div>
+          </div>
         </div>
       </div>
     </div>
