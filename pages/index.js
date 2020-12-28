890ee851b873d9b017f3c68e58e866fd3b15503e
Saeed Hadikhanloo <saeed.hadikhanloo@gmail.com>
2020-12-28 23:38:57 +0100

    create a simple header

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,19 @@
 import Layout from '../components/Layout';
+import Image from 'next/image';
+import styles from '../styles/Home.module.scss';
 
 export default function Home() {
-  return <Layout>Hello</Layout>;
+  return (
+    <Layout>
+      <div className={styles.header}>
+        <div className={styles.headerText}>
+          <div className={styles.title}>Hello</div>
+          <div className={styles.text}>Our company is best and no one can compete with us.</div>
+        </div>
+        <div className={styles.headerImage}>
+          <Image src="/cube1.svg" width="500" height="500" />
+        </div>
+      </div>
+    </Layout>
+  );
 }
