229513372bea22c47c51c89b79f891d9dbc7cc0e
Saeed Hadikhanloo <saeed.hadikhanloo@gmail.com>
2020-12-28 23:03:06 +0100

    new file:   components/Navbar.js

--- pages/index.js
+++ pages/index.js
@@ -1,30 +1,5 @@
-import Link from 'next/link';
-import Image from 'next/image';
-import styles from '../styles/Home.module.scss';
+import Navbar from '../components/Navbar';
 
 export default function Home() {
-  return (
-    <div className={styles.container}>
-      <div className={styles.navbar}>
-        <div className={styles.logo}>
-          <Image src="/favicon.ico" width={500} height={500} />
-        </div>
-        <div className={styles.navbarItems}>
-          <div className={styles.navbarItem}>
-            <Link href="/">
-              <a>Home</a>
-            </Link>
-          </div>
-          <div className={styles.navbarItem}>
-            <Link href="/about">
-              <a>About</a>
-            </Link>
-          </div>
-          <div className={styles.navbarItem}>
-            <div className={styles.btn}>Login</div>
-          </div>
-        </div>
-      </div>
-    </div>
-  );
+  return <Navbar />;
 }
